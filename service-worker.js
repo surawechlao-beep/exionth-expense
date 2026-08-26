91f4caddcb21353436dc5da01f9bf4c86bc07e2b
surawechlao-beep <surawech.lao@gmail.com>
2026-08-26 16:38:07 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * Service Worker for PWA offline cache
  * Strategy: Network First for JS/HTML (always fresh) + Cache First for static assets
  */
-const CACHE_NAME = 'exionth-expense-v28';
+const CACHE_NAME = 'exionth-expense-v29';
 const STATIC_ASSETS = [
   './',
   './index.html',
